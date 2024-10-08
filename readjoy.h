386c666a78c8d88424a0a5a9ce89f7a0f5d60bbc
NotExactlySiev <sadrabousi@gmail.com>
2024-10-08 14:20:16 +0330

    assembly cleanup

--- readjoy.h
+++
@@ -1,6 +0,0 @@
-extern void InitJoy();
-extern unsigned int ReadJoy();
-extern void putchar(char);
-extern int A(char, ...);
-extern void GP0com(unsigned int);
-extern void GP1com(unsigned int);
--- readjoy.s
+++ readjoy.s
@@ -1,75 +1,5 @@
-JOYDATA =     0x1F801040
-JOYSTAT =     0x1F801044
-JOYMODE =     0x1F801048
-JOYCTRL =     0x1F80104A
-JOYBAUD =     0x1F90104E
-
-GP0     =     0x1F801810
-GP1     =     0x1F801814
-
 .set noreorder
 .section .text
-
-.globl InitJoy
-.type InitJoy,@function
-
-InitJoy:
-    li      $t0, 1
-    li      $t1, 0x000D
-    sh      $t0, JOYCTRL
-    sh      $t1, JOYMODE
-    jr      $ra
-    nop
-
-
-.globl ReadJoy
-.type ReadJoy,@function
-
-ReadJoy:
-    li      $a0, 0x42000000
-    li      $a1, 0x01
-
-    # select controller
-    li      $t1, 0x1003
-    nop
-    sh      $t1, JOYCTRL
-
-    # idle for a few hundred cycles
-    li      $t0, 300
-$idle:
-    bne     $t0, $zero, $idle
-    addi    $t0, $t0, -1
- 
-    sw      $a0, 0x0
-    sb      $a1, 0x4
-
-    li      $t1, 5
-$loop:    
-    lb      $t0, -0x1($t1)
-    nop
-    sb      $t0, JOYDATA
-
-$wait:
-    lw      $t0, JOYSTAT
-    nop
-    andi    $t0, $t0, 2
-    beq     $t0, $zero, $wait
-    nop
-
-    lbu     $t0, JOYDATA
-    nop
-    sb      $t0, 0xF($t1)
-    addi    $t1, $t1, -1
-    bne     $t1, $zero, $loop
-
-    li      $t0, 0x0011
-    nop
-    sh      $t0, JOYCTRL
-
-    lw      $v0, 0x10
-
-    jr      $ra
-    nop
 
 .macro sysc code, label
     .globl \label
@@ -99,42 +29,6 @@ kern 0xB0, 0x08, OpenEvent
 kern 0xB0, 0x09, CloseEvent
 kern 0xB0, 0x0C, EnableEvent
 kern 0xB0, 0x0D, DisableEvent
-
-
-.globl putchar
-.type putchar,@function
-
-putchar:
-    li      $t1, 0x3C
-    nop
-    j       0x00A0
-    nop
-    jr      $ra
-    nop
-
-.globl memcpy
-.type memcpy,@function
-
-k_memcpy:
-    li      $t1, 0x2A
-    nop
-    j       0x00A0
-    nop
-    jr      $ra
-    nop
-
-.globl A
-.type A,@function
-
-A:
-    move    $t1, $a0
-    j       0x00A0
-    nop
-    jr      $ra
-    nop
-
-.globl GP0com
-.type GP0com,@function
 
 .globl vec3_cross
 .type vec3_cross,@function
